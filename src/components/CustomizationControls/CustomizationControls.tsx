ede10f76d30e56cd38c62488dd64881aa15ba0c2
nunoplima <nunoplima@hotmail.com>
2024-06-26 13:39:30 +0100

    add textures and model

---
+++ src/components/CustomizationControls/CustomizationControls.tsx
@@ -0,0 +1,7 @@
+export const CustomizationControls = () => {
+  return (
+    <div className="absolute bottom-0 left-0 right-0">
+      <p className="text-3xl font-bold underline">controls</p>
+    </div>
+  )
+}
