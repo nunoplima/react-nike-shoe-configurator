237056c6056cdb6375a3e7eb9e05a590bf0202e1
nunoplima <nunoplima@hotmail.com>
2024-07-09 22:17:49 +0100

    add download btn

---
+++ src/components/Download/Download.tsx
@@ -0,0 +1,9 @@
+import Button from '../ui/Button'
+
+export const Download = () => (
+  <div className="absolute right-0 top-0 px-5 pt-11 font-sans text-sm font-medium md:px-14">
+    <Button classNames="rounded-full bg-white p-2" onClick={() => {}}>
+      <img src="icons/upload.svg" />
+    </Button>
+  </div>
+)
