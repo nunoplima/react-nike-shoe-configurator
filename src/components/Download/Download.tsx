d343d08f05788a57c1903f027d0d6be544637a53
nunoplima <nunoplima@hotmail.com>
2024-07-10 11:56:18 +0100

    add dl screenshot

--- src/components/Download/Download.tsx
+++ src/components/Download/Download.tsx
@@ -1,9 +1,24 @@
 import Button from '../ui/Button'
 
-export const Download = () => (
-  <div className="absolute right-0 top-0 px-5 pt-11 font-sans text-sm font-medium md:px-14">
-    <Button classNames="rounded-full bg-white p-2" onClick={() => {}}>
-      <img src="icons/upload.svg" />
-    </Button>
-  </div>
-)
+export const Download = () => {
+  const handleClick = () => {
+    const link = document.createElement('a')
+    link.setAttribute('download', 'air-jordan-1-by-you.webp')
+    link.setAttribute(
+      'href',
+      document
+        .querySelector('canvas')!
+        .toDataURL('image/webp')
+        .replace('image/webp', 'image/octet-stream'),
+    )
+    link.click()
+  }
+
+  return (
+    <div className="absolute right-0 top-0 px-5 pt-11 font-sans text-sm font-medium md:px-14">
+      <Button classNames="rounded-full bg-white p-2" onClick={handleClick}>
+        <img src="icons/upload.svg" />
+      </Button>
+    </div>
+  )
+}
